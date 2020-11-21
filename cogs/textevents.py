9de0abd7ea39d8bc37aeeacf06876f268fc2bf95
Maxwell Borges-Zulauf <maxwellborgeszulauf@gmail.com>
2020-11-21 00:41:24 -0500

    edited textevents.py to no longeer use levelsystem.py

--- cogs/textevents.py
+++ cogs/textevents.py
@@ -17,7 +17,7 @@ class TextEvents(commands.Cog):
                 and message.author != self.bot.user:
             await message.add_reaction(':Pog:698454205744152628')
 
-        if not message.author.bot:
+        '''if not message.author.bot:
             with open('users.json', 'r') as f:
                 users = json.load(f)
 
@@ -28,7 +28,7 @@ class TextEvents(commands.Cog):
             await levelsystem.check_for_level_up(users, message.author, message.channel)
 
             with open('users.json', 'w') as f:
-                json.dump(users, f)
+                json.dump(users, f)'''
 
         # await self.bot.process_commands(message)
 
@@ -45,7 +45,7 @@ class TextEvents(commands.Cog):
         if member.guild.id == 643645122638249985:  # avacado server
             await member.add_roles(member.guild.get_role(717104002021589113))  # avacado role
 
-        if not member.bot:
+        '''if not member.bot:
             with open('users.json', 'r') as f:
                 users = json.load(f)
 
@@ -54,7 +54,7 @@ class TextEvents(commands.Cog):
             await levelsystem.update_data(users, member)
 
             with open('users.json', 'w') as f:
-                json.dump(users, f)
+                json.dump(users, f)'''
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
