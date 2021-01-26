cb35cd95ddc2847ab8c44c5e708e8c1515021a82
Maxarino <maxwellborgeszulauf@gmail.com>
2021-01-26 14:00:54 -0500

    Added back level system

--- cogs/textevents.py
+++ cogs/textevents.py
@@ -16,8 +16,10 @@ class TextEvents(commands.Cog):
         if 'pog' in message.content.lower().replace(" ", "") \
                 and message.author != self.bot.user:
             await message.add_reaction(':Pog:698454205744152628')
+        elif 'ratio' in message.content:
+            await message.add_reaction(':heart:')
 
-        '''if not message.author.bot:
+        if not message.author.bot:
             with open('users.json', 'r') as f:
                 users = json.load(f)
 
@@ -28,7 +30,7 @@ class TextEvents(commands.Cog):
             await levelsystem.check_for_level_up(users, message.author, message.channel)
 
             with open('users.json', 'w') as f:
-                json.dump(users, f)'''
+                json.dump(users, f)
 
         # await self.bot.process_commands(message)
 
@@ -45,7 +47,7 @@ class TextEvents(commands.Cog):
         if member.guild.id == 643645122638249985:  # avacado server
             await member.add_roles(member.guild.get_role(717104002021589113))  # avacado role
 
-        '''if not member.bot:
+        if not member.bot:
             with open('users.json', 'r') as f:
                 users = json.load(f)
 
@@ -54,7 +56,7 @@ class TextEvents(commands.Cog):
             await levelsystem.update_data(users, member)
 
             with open('users.json', 'w') as f:
-                json.dump(users, f)'''
+                json.dump(users, f)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
